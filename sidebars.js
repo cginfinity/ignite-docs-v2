2c6fc27b07ab3aa827ad9ce82028fcba57f5bf32
Ravi-Kaushish <ravikantsharma2169@gmail.com>
2021-03-10 20:36:23 +0530

    added docs v2 boiler palte

---
+++ sidebars.js
@@ -0,0 +1,99 @@
+module.exports = {
+  someSidebar: {
+    "Getting Started": [
+      "getting-started/introduction",
+      "getting-started/what-is-ignite",
+      {
+        "Try Ignite": [
+          "getting-started/try-ignite/deep-dive",
+          {
+            "Deploy to AWS":[
+              'doc1',
+            ],
+            "Deploy to Azure":[
+              'doc1',
+            ],
+            "Deploy to Heroku":[
+              'doc1',
+            ]
+          }
+        ]
+      }
+    ],
+    "Ignite Ecosystem": [
+      {
+        "Runtime": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Editor": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Monitor": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Dashboard": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Designer": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Explorer": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ]
+      }
+    ],
+    "Developer Guide": [
+      {
+        "Basics": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ],
+        "Advanced": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ]
+      }
+    ],
+    "Tutorials": [
+      {
+        "abcd": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ]
+      }
+    ],
+    "Connectors": [
+      {
+        "abcd": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ]
+      }
+    ],
+    "Devops Practices": [
+      {
+        "abcd": [
+          'doc1',
+          'doc2',
+          'doc3'
+        ]
+      }
+    ]
+  }
+};
